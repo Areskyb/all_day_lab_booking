b9ec7200d5195252bdc592cdb06cac4f5e2d11c4
Aresky Berkane <areskyberkane@gmail.com>
2019-12-04 11:49:13 +0000

    create index route and show route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 
-const createRouter = function (collection){
+const createRouter = function (collection) {
   
   const router = express.Router();
   
-  router.get('/',(req,res) =>{
+  router.get('/', (req,res) => {
     collection
     .find()
     .toArray()
@@ -15,6 +15,21 @@ const createRouter = function (collection){
       res.status(500);
       res.json({status: 500, error: err});
     });
-  })
+  });
   
-}
+  router.get('/:id',(req,res) => {
+    const id = req.params.id;
+    collection
+    .findOne({_id: ObjectID(id)})
+    .then((doc) => res.json(doc))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({status: 500, error: err});
+    });
+  });
+  
+  return router;
+};
+
+module.exports = createRouter;
