bde2f042cd35131cdae36d3d36abd85523289d8f
Aresky Berkane <areskyberkane@gmail.com>
2019-12-04 14:00:56 +0000

    add grid html and props

---
+++ client/src/components/GuestGrid.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div id="guestGrid">
+    <div v-for="guest in guests">
+      <h2>{{ guest.name }}</h2>
+      <p>{{ guest.email }} </p>
+      <p> Status: {{ guest.checkedStatus }}</p>
+      <button type="button" name="button">Delete Guest Booking</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'guest-grid',
+  props: ["guests"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
