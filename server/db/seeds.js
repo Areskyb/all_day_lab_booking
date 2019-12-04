b79b3861db5af3748d8c0c22f315a5e2679bd524
Aresky Berkane <areskyberkane@gmail.com>
2019-12-04 14:46:47 +0000

    altered check in status phrasing

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -5,21 +5,21 @@ db.guests.insertMany([
   {
     name:"Juan",
     email:"juan@codeclan.com",
-    checkedStatus:"true"
+    checkedStatus:"checked in"
   },
   {
     name:"Eugene",
     email:"eugene@codeclan.com",
-    checkedStatus:"false"
+    checkedStatus:"not checked in"
   },
   {
     name:"Sandy",
     email:"sandy@codeclan.com",
-    checkedStatus:"true"
+    checkedStatus:"checked in"
   },
   {
     name:"Colin",
     email:"colin@codeclan.com",
-    checkedStatus:"false"
+    checkedStatus:"not checked in"
   }
 ]);
